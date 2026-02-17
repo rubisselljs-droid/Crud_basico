5cc2873741637419627f97cbfbfc679c8c873db3
rubisselljs-droid <rubisselljs@gmail.com>
2026-02-17 15:07:44 -0600

    tabla simple

---
+++ CrudB/src/createClient.js
@@ -0,0 +1,6 @@
+import {createClient} from '@supabase/supabase-js';
+
+
+export const supabase = createClient('https://nkhbhbsgtoskzdalsphu.supabase.co',
+    'sb_publishable_3Kk1RSYFQNp6-SgI_cZiJA_cK6izYzi'
+);
